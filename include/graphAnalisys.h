09463f39830049355bf26532cadcf0d4d7d59f6e
Mattia Muti <mat.muti@stud.uniroma3.it>
2021-04-09 12:25:03 +0200

    refactoring

--- include/graphAnalisys.h
+++ include/graphAnalisys.h
@@ -2,36 +2,30 @@
 #include <ogdf/fileformats/GraphIO.h>
 #include <string>
 #include <vector>
-#include <set>
 
 using namespace ogdf;
 
+//creazione grafi randomici
 int createRandomGraphGML(std::string path, int nodes, int edges, bool connected);
 
-int connectivityAnalisys(Graph G);
-
-std::vector<node> connectedCompMax(Graph G);
-
-std::vector<node> biconnectedCompMax(Graph G);
-
+//funzioni per la visualizzazione
 int fmmmLayout(std::string readPath, std::string writePathGML, std::string writePathSVG);
-
 int fmmmLayout(Graph G, std::string writePathGML, std::string writePathSVG);
-
-int bctreeVisualization(std::string readPath, std::string writePathGML, std::string writePathSVG);
-
 int dhLayout(Graph G, std::string writePathGML, std::string writePathSVG);
-
 int dhLayout(std::string readPath, std::string writePathGML, std::string writePathSVG);
 
-void DFSUtil(node n, std::vector<bool> &visited, std::vector<int> &vDegree, int k);
+//funzioni per l'analisi della connettività
+Graph connectivity(Graph G);
+Graph chooseComponentMax(Graph G);
+int connectivityAnalisys(Graph G);
+std::vector<node> connectedCompMax(Graph G);
+std::vector<node> biconnectedCompMax(Graph G);
 
-void kcore(Graph * G, int k);
+//funzioni sui BC-Tree
+std::vector<node> cutVertexAnalisys(Graph G);
+void cvRatio(std::vector<node> cvG, std::vector<node> cvkG);
+int bctreeVisualization(std::string readPath, std::string writePathGML, std::string writePathSVG);
 
-Graph kcore(Graph G, int k);
-
-void kcore_analisys(std::string path, int k);
-
-void nodeLabelling(std::string path);
-
-int procedure(std::string path);
+//funzioni per il calcolo della coreness
+std::vector<int> nodeLabelling(Graph G);
+void kcore(Graph * G, std::vector<int> labels, int k);
