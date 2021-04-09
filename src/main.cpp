55481c4b907f34e92b05ada0e70d31c1efe1f5ea
Mattia Muti <mat.muti@stud.uniroma3.it>
2021-04-09 12:27:28 +0200

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,43 @@
 #include <ogdf/fileformats/GraphIO.h>
+#include <ogdf/basic/simple_graph_alg.h>
 #include <ogdf/decomposition/BCTree.h>
 #include "../include/graphAnalisys.h"
 
 using namespace ogdf;
 using namespace std;
 
-int main(string path)
+int main()
 {
+    //lettura grafo
     Graph G;
-    if (!GraphIO::read(G,path)) {
+    if (!GraphIO::read(G,"gml/amazon.gml")) {
         std::cerr << "impossibile aprire il file" << std::endl;
         return 1;
     }
-
-    vector<node> lista;
-    for (node n : G.nodes)
-    {
-        lista.push_back(n);
-    }
-
-    Graph g2core = G;
+    vector<node> cvG;                       //vertici di taglio di G
+    vector<node> cvkG;                      //vertici di taglio del k-core di G
+    vector<int> labels = nodeLabelling(G);  //etichette coreness
     
-    connectivityAnalisys(G);
+    //analisi grafo iniziale
+    G = connectivity(G);
+    cvG = cutVertexAnalisys(G);
 
-
-    /*  costruisco il sottografo indotto dalla 
-        componente biconnessa più grande    */
-    vector<node> nodes = biconnectedCompMax(G);
-    for(node n : lista)
-    {
-        if(find( begin(nodes), end(nodes), n) == end(nodes))
-        {
-            g2core.delNode(n);
-        }
-    }
-
-    Graph g3core = g2core;
-    g3core = kcore(g3core,3);
-
-    connectivityAnalisys(g3core);
+    //analisi 2-core
+    Graph kG = G;
+    Graph *p = &kG;
+    
+    kcore(p, labels, 2);
+    cout << endl << "2-CORE" << endl;
+    connectivity(kG);
+    cvkG = cutVertexAnalisys(kG);
+    cvRatio(cvG, cvkG);
+    
+    //analisi 3 core
+    kcore(p, labels, 3);
+    cout << endl << "3-CORE" << endl;
+    kG = connectivity(kG);
+    cvkG = cutVertexAnalisys(kG);
+    cvRatio(cvG, cvkG);
 
     return 1;
 }
