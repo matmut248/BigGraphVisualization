503c9d232eb67cd3faa9792c54f565dbff410344
matmut248 <mat.muti@stud.uniroma3.it>
2021-05-15 15:58:53 +0200

    risolti bug

--- graph_tool/src/report.py
+++ graph_tool/src/report.py
@@ -16,12 +16,14 @@ def procedure(g):
             file.write("il grafo ha " + str(g.num_vertices()) + " nodi e " + str(g.num_edges()) + " archi\n")
             comp, hist = gt.label_components(g)
             bcomp, cv, num = gt.label_biconnected_components(g)
+            tot_cv = len(list(filter(lambda x: (x == 1), cv)))
             file.write("numero di componenti connesse: " + str(len(hist)) + "\n")
             file.write("numero di componenti biconnesse: " + str(len(num)) + "\n")
-            file.write("numero di vertici di taglio: " + str(len(list(filter(lambda x: (x == 1), cv)))) + "\n")
+            file.write("numero di vertici di taglio: " + str(tot_cv) + "\n")
+            if tot_cv == 0:
+                return
             # per ogni componente connessa
             for i in range(len(hist)):
-                file.write("\t------ analisi della componente connessa #" + str(i) + " ------\n")
                 curr_comp = g.new_vp("bool")
                 num_cv = 0
                 for v in g.vertices():
@@ -29,14 +31,18 @@ def procedure(g):
                         curr_comp[v] = True
                         if cv[v] == 1:
                             num_cv += 1
-                sub_g = gt.GraphView(g,curr_comp)
-                file.write("\tnumero di nodi: " + str(sub_g.num_vertices()) + "\n")
-                file.write("\tnumero di archi: " + str(sub_g.num_edges()) + "\n")
-                file.write("\tnumero di vertici di taglio: " + str(num_cv) + "\n")
-                # calcolo bctree
-                bct = bc.BCTree(sub_g, adj_matrix, cv)
-                file.write("\tnumero blocchi (NODI B) del BC-Tree: " + str(bct.num_Bcomp) + "\n")
-                file.write("\tnumero vertici di taglio (NODI C) del BC-Tree: " + str(bct.num_Ccomp) + "\n")
+                #sono di interesse solo componenti con numero di cv > dello 0,1% dei cv totali
+                if num_cv > (tot_cv/1000):
+                    file.write("\n\t------ analisi della componente connessa #" + str(i) + " ------\n")
+                    sub_g = gt.GraphView(g, curr_comp)
+                    file.write("\tnumero di nodi: " + str(sub_g.num_vertices()) + "\n")
+                    file.write("\tnumero di archi: " + str(sub_g.num_edges()) + "\n")
+                    file.write("\tnumero di vertici di taglio: " + str(num_cv) + "\n")
+                    # calcolo bctree
+                    bct = bc.BCTree(sub_g, adj_matrix)
+                    file.write("\tnumero nodi del BC-Tree: " + str(bct.bctree.num_vertices()) + "\n")
+                    file.write("\tnumero blocchi (NODI B) del BC-Tree: " + str(bct.num_Bcomp) + "\n")
+                    file.write("\tnumero vertici di taglio (NODI C) del BC-Tree: " + str(bct.num_Ccomp) + "\n")
 
             k += 1
 
@@ -49,10 +55,12 @@ def k_core_filtering(g, kval, k):
             vpCore[v] = True
     g.set_vertex_filter(vpCore)
 
-g = gt.load_graph("../gml/youtube.gml")
+g = gt.load_graph("../gml/amazon.gml")
+
+
 
 gp_name = g.new_gp("string")
 g.gp["name"] = gp_name
-g.gp["name"] = "youtube"
+g.gp["name"] = "amazon"
 bga.preprocessing(g)
 procedure(g)
