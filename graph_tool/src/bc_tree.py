b854314f12054e227d000077f038359865372a57
matmut248 <mat.muti@stud.uniroma3.it>
2021-05-30 10:25:25 +0200

    fix bug

--- graph_tool/src/bc_tree.py
+++ graph_tool/src/bc_tree.py
@@ -17,7 +17,6 @@ class BCTree:
 
         self.init_bctree(g, adj_matrix)
 
-        #self.draw_bctree()
 
     def init_bctree(self, g, adj_matrix):
 
@@ -40,6 +39,7 @@ class BCTree:
                     e = g.edge(temp,w)
                 except ValueError:
                     continue
+
                 if bcomp2bc_nodes[bcomp[e]] is None:            #se non ho già visitato la bcomp
                     new_bcomp_node = self.bctree.add_vertex()   #new_bcomp_node è una componente B del bctree
                     self.node2isBcomp[new_bcomp_node] = 1
@@ -47,6 +47,7 @@ class BCTree:
                     bcomp2bc_nodes[bcomp[e]] = new_bcomp_node   # new_bcomp_node corrisponde a bcomp[e]
                 else:
                     new_bcomp_node = self.bctree.vertex(bcomp2bc_nodes[bcomp[e]])   #prendo il nodo corrispondente a bcomp[e]
+
                 if self.bctree.edge(v, new_bcomp_node) is None:
                     self.bctree.add_edge(v,new_bcomp_node)      #aggiungo l'arco tra v e new_bcomp_node se manca
                 if cv[w] == 0:
@@ -61,3 +62,16 @@ class BCTree:
                 self.gNode2bcNode[t] = new_bcomp_node
                 self.node2isBcomp[new_bcomp_node] = 1
                 self.num_Bcomp += 1
+                bcomp2bc_nodes[bcomp[e]] = new_bcomp_node
+
+
+G = gt.Graph()
+G.set_directed(False)
+#G.add_edge_list([(0, 1), (0, 2), (0, 3), (0, 4), (3, 4), (4,5), (0,6),(3,6),(4,6)])
+G.add_edge_list([(0,1),(0,2),(1,2),(0,4),(4,5),(4,6),(4,7),(5,6),(5,7),(6,7),(6,8),(8,9),(8,14),(9,10),
+                     (9,11),(9,12),(10,11),(10,12),(11,12),(12,13),(14,15),(14,16),(14,17),(15,16),
+                     (15,17),(16,17),(17,3), (18,19),(19,20),(18,20), (20,21), (22,23)])
+adj = gt.adjacency(G)
+bc = BCTree(G,adj)
+
+#gt.graph_draw(bc.bctree)
